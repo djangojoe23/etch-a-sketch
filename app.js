5fe51cf6d3fe39d82e269880984d7486f34d1fec
Joe Meyer <17205605+djangojoe23@users.noreply.github.com>
2024-09-27 04:57:25 -0400

    Dynamically created the grid in javascript.

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("hello")
+
+window.onload = function() {
+    let container = document.createElement("div");
+    container.setAttribute('id', 'container');
+    document.body.appendChild(container);
+
+    let row = null;
+    let cell = null;
+    for (let i=0; i<16; i++){
+        row = document.createElement("div");
+        row.setAttribute('class', 'row');
+        container.appendChild(row);
+        for (let j=0; j<16; j++){
+            cell = document.createElement("div");
+            cell.setAttribute('class', 'cell');
+            row.appendChild(cell);
+        }
+    }
+}
